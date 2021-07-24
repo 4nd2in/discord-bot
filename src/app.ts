8c541d4df621fe32ed7c415f554f36d56d15da14
Andrin Weiler <andrin.weiler@smoca.ch>
2021-07-24 17:17:42 +0200

    refactor

--- src/app.ts
+++ src/app.ts
@@ -1,44 +1,15 @@
 import dotenv from "dotenv";
 import Discord from "discord.js";
-import fs from "fs";
-import { config } from "./config";
-import { Command } from "./commands/Command";
-import { handleError } from "./helper/handleError";
+import { handleOnMessage } from "./helper/handleOnMessage";
 
 const client = new Discord.Client();
-const commands = new Discord.Collection<string, Command>();
 
-const commandFiles = fs
-  .readdirSync("./commands")
-  .filter((file) => file.endsWith(".ts"));
-
-for (const file of commandFiles) {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const command: Command = require(`./commands/${file}`);
-  commands.set(command.name, command);
-}
 dotenv.config();
 
 client.once("ready", () => {
   console.log("ready");
 });
 
-client.on("message", (message) => {
-  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
-
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/);
-  const command = args?.shift()?.toLowerCase();
-
-  if (command === undefined) {
-    handleError("Given Command is empty.", message);
-    return false;
-  }
-  if (!commands.has(command)) {
-    handleError(`Command named ${command} does not exist.`, message);
-    return;
-  }
-
-  commands?.get(command)?.execute(message, args);
-});
+client.on("message", (message) => handleOnMessage(message));
 
 client.login(process.env.TOKEN);
---
+++ src/helper/handleOnMessage.ts
@@ -0,0 +1,35 @@
+import Discord from "discord.js";
+import fs from "fs";
+import { Command } from "src/commands/Command";
+import { config } from "src/config";
+import { handleError } from "./handleError";
+
+const commands = new Discord.Collection<string, Command>();
+
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".ts"));
+
+for (const file of commandFiles) {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const command: Command = require(`./commands/${file}`);
+  commands.set(command.name, command);
+}
+
+export const handleOnMessage = (message: Discord.Message): void => {
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/);
+  const command = args?.shift()?.toLowerCase();
+
+  if (command === undefined) {
+    handleError("Given Command is empty.", message);
+    return;
+  }
+  if (!commands.has(command)) {
+    handleError(`Command named ${command} does not exist.`, message);
+    return;
+  }
+
+  commands?.get(command)?.execute(message, args);
+};
