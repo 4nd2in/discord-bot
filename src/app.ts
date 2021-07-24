37a29529055eedbb23de8080e940ca08faa45dd0
Andrin Weiler <andrin.weiler@smoca.ch>
2021-07-24 14:08:15 +0200

    added token

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log("Hello");
+import dotenv from "dotenv";
+import Discord from "discord.js";
+
+const client = new Discord.Client();
+dotenv.config();
+
+client.once("ready", () => {
+  console.log("ready");
+});
+
+client.login(process.env.TOKEN);
