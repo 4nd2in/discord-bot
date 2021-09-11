c008728ea9d4e9c6618c9ecbe62dcfad58659afb
Andrin Weiler <andrin.weiler@smoca.ch>
2021-09-11 16:10:18 +0200

    using dotenv to deploy commands

--- src/config.ts
+++
@@ -1,4 +0,0 @@
-export const config = {
-  clientId: "293819274701570049",
-  guildId: "401442411978686495",
-};
--- src/helper/deploy.ts
+++ src/helper/deploy.ts
@@ -1,24 +1,26 @@
 import dotenv from "dotenv";
 import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v9";
-import { config } from "../config.js";
 import { commands } from "./commands.js";
 
 dotenv.config();
-if (process.env.TOKEN) {
-  const rest = new REST({ version: "9" }).setToken(process.env.TOKEN);
+if (process.env.TOKEN && process.env.CLIENT && process.env.GUILD) {
+  const token = process.env.TOKEN;
+  const client = process.env.CLIENT;
+  const guild = process.env.GUILD;
+
+  const rest = new REST({ version: "9" }).setToken(token);
   (async (): Promise<void> => {
     try {
-      await rest.put(
-        Routes.applicationGuildCommands(config.clientId, config.guildId),
-        {
-          body: commands.map((command) => command.data.toJSON()),
-        }
-      );
+      await rest.put(Routes.applicationGuildCommands(client, guild), {
+        body: commands.map((command) => command.data.toJSON()),
+      });
 
       console.log("Successfully registered application commands.");
     } catch (error) {
       console.error(error);
     }
   })();
+} else {
+  console.log("One or more needed values are not set in your .env file!");
 }
