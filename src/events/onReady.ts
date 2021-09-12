2212c01e0773045bf9c8f5693aa9bb114984b082
Andrin Weiler <andrin.weiler@smoca.ch>
2021-09-12 15:42:14 +0200

    synchronize database

--- src/events/onReady.ts
+++ src/events/onReady.ts
@@ -1,9 +1,16 @@
 import { Client } from "discord.js";
+import { sequelizeClient } from "../database/SequelizeClient";
+
+const synchronizeDatabse = async (): Promise<void> => {
+  const model = await sequelizeClient.sync();
+  console.log("Model synchronization", model ? "was successful" : "failed");
+};
 
 export default {
   name: "ready",
   once: true,
   execute(client: Client): void {
+    synchronizeDatabse();
     console.log(`Ready! Logged in as ${client.user?.tag}`);
   },
 };
