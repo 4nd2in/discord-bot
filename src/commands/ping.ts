839ecca1781d5831511b53b0d4f9f3fae8e91f2e
4nd2in <andrin.weiler@smoca.ch>
2024-05-15 16:17:46 +0200

    Fixed import and naming

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,12 +1,12 @@
 import { CacheType, CommandInteraction, SlashCommandBuilder } from "discord.js";
-import { Command } from "../interfaces/command";
+import { Command } from "../interfaces/Command";
 
 const ping: Command = {
     data: new SlashCommandBuilder()
         .setName("ping")
-        .setDescription("Replies with pong!"),
+        .setDescription("Simple command to test if the bot is responding to interactions."),
     run: async (interaction: CommandInteraction<CacheType>) => {
-        await interaction.reply("pong!");
+        await interaction.reply("pong!").catch(console.error);
         console.info(`Sent 'pong!' to channel ${interaction?.channel?.id}`);
     }
 };
--- src/config.ts
+++ src/config.ts
@@ -6,8 +6,6 @@ if (!process.env.TOKEN) {
     console.warn("Missing Discord bot token");
 }
 
-const config = {
+export const config = {
     token: process.env.TOKEN as string
 };
-
-export default config;
