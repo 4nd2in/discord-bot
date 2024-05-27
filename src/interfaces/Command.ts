673388fed7d6d0743cad1b4d1893f75c8bf3ede6
4nd2in <andrin.weiler@smoca.ch>
2024-05-27 17:50:57 +0200

    Added Options Only builder to command data type

--- src/interfaces/Command.ts
+++ src/interfaces/Command.ts
@@ -1,10 +1,11 @@
 import {
     CommandInteraction,
     SlashCommandBuilder,
+    SlashCommandOptionsOnlyBuilder,
     SlashCommandSubcommandsOnlyBuilder
 } from "discord.js";
 
 export interface Command {
-    data: SlashCommandBuilder | SlashCommandSubcommandsOnlyBuilder;
+    data: SlashCommandBuilder | SlashCommandOptionsOnlyBuilder | SlashCommandSubcommandsOnlyBuilder;
     run: (interaction: CommandInteraction) => Promise<void>;
 }
